adf98328f46235a9482695a9ade647af3682601f
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-03-03 16:01:47 -0500

    reverting

--- ui/pages/home/home.component.js
+++ ui/pages/home/home.component.js
@@ -330,7 +330,7 @@ export default class Home extends PureComponent {
             }
           />
         ) : null}
-        {newNetworkAdded || true ? (
+        {newNetworkAdded ? (
           <ActionableMessage
             type="info"
             className="home__new-network-notification"
